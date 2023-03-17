5b12d24e99cfdcadca113047100b7d66bf9297d2
Malargasop <126952102+Malargasop@users.noreply.github.com>
2023-03-17 15:05:53 +0300

    добавил папку с решениями задач 19, 21,23

---
+++ Examples 19 21 23/Решение задачи 19.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int x = number.Length;
+
+if (x == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - Не является пятизначным числом ");
+}
---
+++ Examples 19 21 23/Решение задачи 23.cs
@@ -0,0 +1,8 @@
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= x; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+
